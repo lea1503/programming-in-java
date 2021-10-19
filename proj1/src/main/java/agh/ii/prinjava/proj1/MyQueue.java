370c489ef9c43ba468ceb39d00b3d9b4c344a24d
leala <lea.lauthrey@gmail.com>
2021-10-19 23:16:47 +0200

    Prjt1 project commit

--- proj1/src/main/java/agh/ii/prinjava/proj1/MyQueue.java
+++ proj1/src/main/java/agh/ii/prinjava/proj1/MyQueue.java
@@ -3,16 +3,37 @@ package agh.ii.prinjava.proj1;
 import agh.ii.prinjava.proj1.impl.MyQueueDLLBImpl;
 
 public interface MyQueue<E> {
+
+    /**
+     * enqueue is a method that adds an element at the end of the queue.
+     * @param x, generic type E. The value of the added element.
+     */
     void enqueue(E x);
 
+    /**
+     * dequeue is a method that deletes an element at the beginning of the queue.
+     * @return E, generic type, the value of the deleted element.
+     */
     E dequeue();
 
+    /**
+     * isEmpty is a boolean method that restart numOfElems at 0 if the queue is empty.
+     * @return boolean. If isEmpty, then numOfElems is equal to 0.
+     */
     default boolean isEmpty() {
         return numOfElems() == 0;
     }
 
+    /**
+     * numOfElems is a method that returns the number of element in the queue.
+     * @return integer.
+     */
     int numOfElems();
 
+    /**
+     * peek is a method that returns the value of the first element of the queue.
+     * @return E, generic type.
+     */
     E peek();
 
     /** Consider pros and cons of having a factory method in the interface */
--- proj1/src/main/java/agh/ii/prinjava/proj1/MyStack.java
+++ proj1/src/main/java/agh/ii/prinjava/proj1/MyStack.java
@@ -3,16 +3,37 @@ package agh.ii.prinjava.proj1;
 import agh.ii.prinjava.proj1.impl.MyStackDLLBImpl;
 
 public interface MyStack<E> {
+
+    /**
+     * pop is a method that deletes an element at the beginning of the stack.
+     * @return E, generic type. The value of the deleted element.
+     */
     E pop();
 
+    /**
+     * push is a method that adds an element at the beginning of the stack.
+     * @param x, generic type E, the value of the added element.
+     */
     void push(E x);
 
+    /**
+     * isEmpty is a boolean method that restart numOfElems at 0 if the queue is empty.
+     * @return boolean. If isEmpty, then numOfElems is equal to 0.
+     */
     default boolean isEmpty() {
         return numOfElems() == 0;
     }
 
+    /**
+     * numOfElems is a method that returns the number of element in the queue.
+     * @return integer.
+     */
     int numOfElems();
 
+    /**
+     * peek is a method that returns the value of the first element of the stack.
+     * @return E, generic type.
+     */
     E peek();
 
     /** Consider pros and cons of having a factory method in the interface */
